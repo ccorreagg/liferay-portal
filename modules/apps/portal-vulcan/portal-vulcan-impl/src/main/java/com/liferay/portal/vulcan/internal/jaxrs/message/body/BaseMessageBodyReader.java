7e62061d4402ae124974674683cb065191c136a7
Javier de Arcos <javier.dearcos@liferay.com>
2022-06-16 13:14:58 +0200

    LPS-153319 Modify the BaseMessageBodyReader to take into account the extended properties when the ExtensionContext for the class exists

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/message/body/BaseMessageBodyReader.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/message/body/BaseMessageBodyReader.java
@@ -14,18 +14,27 @@
 
 package com.liferay.portal.vulcan.internal.jaxrs.message.body;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.vulcan.internal.jaxrs.validation.ValidationUtil;
+import com.liferay.portal.vulcan.jaxrs.context.ExtensionContext;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
@@ -35,6 +44,7 @@ import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.Providers;
 
@@ -66,21 +76,69 @@ public abstract class BaseMessageBodyReader
 			InputStream inputStream)
 		throws IOException {
 
-		ObjectReader objectMapper = _getObjectMapper(
-			clazz
-		).readerFor(
-			clazz
-		);
+		ObjectMapper objectMapper = _getObjectMapper(clazz);
 
-		Object value = objectMapper.readValue(inputStream);
+		ObjectReader objectReader = objectMapper.readerFor(clazz);
+
+		ExtensionContext extensionContext = _getExtensionContext(
+			clazz, mediaType);
+
+		if (extensionContext == null) {
+			return objectReader.readValue(inputStream);
+		}
+
+		JsonNode jsonNode = objectReader.readTree(inputStream);
+
+		objectReader = objectReader.without(
+			DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+		Object object = objectReader.readValue(jsonNode);
+
+		_setExtendedProperties(
+			clazz, extensionContext, jsonNode, object, objectMapper);
 
 		if (!StringUtil.equals(
 				_httpServletRequest.getMethod(), HttpMethod.PATCH)) {
 
-			ValidationUtil.validate(value);
+			ValidationUtil.validate(object);
 		}
 
-		return value;
+		return object;
+	}
+
+	private ExtensionContext _getExtensionContext(
+		Class<?> clazz, MediaType mediaType) {
+
+		ContextResolver<ExtensionContext> extensionContextContextResolver =
+			_providers.getContextResolver(ExtensionContext.class, mediaType);
+
+		return extensionContextContextResolver.getContext(clazz);
+	}
+
+	private Object _getJsonNodeValue(
+			JsonNode jsonNode, ObjectMapper objectMapper)
+		throws IOException {
+
+		if (jsonNode.isBoolean()) {
+			return jsonNode.asBoolean();
+		}
+		else if (jsonNode.isDouble()) {
+			return jsonNode.asDouble();
+		}
+		else if (jsonNode.isInt()) {
+			return jsonNode.asInt();
+		}
+		else if (jsonNode.isLong()) {
+			return jsonNode.asLong();
+		}
+		else if (jsonNode.isTextual()) {
+			return jsonNode.asText();
+		}
+		else if (jsonNode.isObject()) {
+			return objectMapper.readValue(jsonNode.traverse(), Object.class);
+		}
+
+		return null;
 	}
 
 	private ObjectMapper _getObjectMapper(Class<?> clazz) {
@@ -92,6 +150,35 @@ public abstract class BaseMessageBodyReader
 			() -> new InternalServerErrorException(
 				"Unable to generate object mapper for class " + clazz)
 		);
+	}
+
+	private void _setExtendedProperties(
+			Class<?> clazz, ExtensionContext extensionContext,
+			JsonNode jsonNode, Object object, ObjectMapper objectMapper)
+		throws IOException {
+
+		List<String> clazzFieldNames = new ArrayList<>();
+
+		for (Field field : clazz.getDeclaredFields()) {
+			clazzFieldNames.add(field.getName());
+		}
+
+		Iterator<String> fieldNamesIterator = jsonNode.fieldNames();
+		Map<String, Object> extendedProperties = new HashMap<>();
+
+		while (fieldNamesIterator.hasNext()) {
+			String jsonFieldName = fieldNamesIterator.next();
+
+			if (!clazzFieldNames.contains(jsonFieldName)) {
+				JsonNode fieldNameJsonNode = jsonNode.get(jsonFieldName);
+
+				extendedProperties.put(
+					jsonFieldName,
+					_getJsonNodeValue(fieldNameJsonNode, objectMapper));
+			}
+		}
+
+		extensionContext.setExtendedProperties(object, extendedProperties);
 	}
 
 	private final Class<? extends ObjectMapper> _contextType;
