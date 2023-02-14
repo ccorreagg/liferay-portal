0c4507473e7623947b4e3796bbced78f3ef63fdb
Carlos Correa <carlos.correa@liferay.com>
2023-02-14 19:48:00 +0100

    Add implementation for the registry of the manager

---
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/ObjectRelationshipManagerRegistryImpl.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.rest.internal.manager.v1_0;
+
+import com.liferay.object.rest.manager.v1_0.ObjectRelationshipManager;
+import com.liferay.object.rest.manager.v1_0.ObjectRelationshipManagerRegistry;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.petra.string.StringPool;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Carlos Correa
+ */
+@Component(service = ObjectRelationshipManagerRegistry.class)
+public class ObjectRelationshipManagerRegistryImpl
+	implements ObjectRelationshipManagerRegistry {
+
+	@Override
+	public ObjectRelationshipManager getObjectRelationshipManager(
+			String className, String type)
+		throws Exception {
+
+		String key = _getKey(className, type);
+
+		ObjectRelationshipManager objectRelationshipManager =
+			_serviceTrackerMap.getService(key);
+
+		if (objectRelationshipManager == null) {
+			throw new IllegalArgumentException(
+				"No object relationship manager found with key " + key);
+		}
+
+		return objectRelationshipManager;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, ObjectRelationshipManager.class, null,
+			(serviceReference, emitter) -> {
+				ObjectRelationshipManager objectRelationshipManager =
+					bundleContext.getService(serviceReference);
+
+				emitter.emit(
+					_getKey(
+						objectRelationshipManager.getClassName(),
+						objectRelationshipManager.getObjectRelationshipType()));
+			});
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private String _getKey(String className, String type) {
+		return className + StringPool.POUND + type;
+	}
+
+	private ServiceTrackerMap<String, ObjectRelationshipManager>
+		_serviceTrackerMap;
+
+}
