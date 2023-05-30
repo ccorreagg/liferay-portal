0c7d3b2f4d6ce09a053fca72f8f84e7b01256e3b
Carlos Correa <carlos.correa@liferay.com>
2023-05-30 15:44:14 +0200

    buildREST

---
+++ modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/dto/v1_0/action/metadata/BaseTaxonomyCategoryDTOActionMetadataProvider.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.taxonomy.internal.dto.v1_0.action.metadata;
+
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.action.ActionInfo;
+
+import java.util.Set;
+
+/**
+ * @author Javier Gamarra
+ * @generated
+ */
+public abstract class BaseTaxonomyCategoryDTOActionMetadataProvider {
+
+	protected String getDeleteActionKey() {
+		return ActionKeys.DELETE;
+	}
+
+	protected String getDeleteResourceMethodName() {
+		return "deleteTaxonomyCategory";
+	}
+
+	protected String getGetActionKey() {
+		return ActionKeys.VIEW;
+	}
+
+	protected String getGetResourceMethodName() {
+		return "getTaxonomyCategory";
+	}
+
+	protected String getReplaceActionKey() {
+		return ActionKeys.UPDATE;
+	}
+
+	protected String getReplaceResourceMethodName() {
+		return "putTaxonomyCategory";
+	}
+
+	public String getPermissionName() {
+		return null;
+	}
+
+	public Set<String> getActionNames() {
+		return SetUtil.fromArray("delete", "get", "replace");
+	}
+
+	public ActionInfo getActionInfo(String actionName) {
+		ActionInfo actionInfo = null;
+
+		if (StringUtil.equals(actionName, "delete")) {
+			actionInfo = new ActionInfo();
+
+			actionInfo.setActionKey(getDeleteActionKey());
+			actionInfo.setResourceMethodName(getDeleteResourceMethodName());
+		}
+		else if (StringUtil.equals(actionName, "get")) {
+			actionInfo = new ActionInfo();
+
+			actionInfo.setActionKey(getGetActionKey());
+			actionInfo.setResourceMethodName(getGetResourceMethodName());
+		}
+		else if (StringUtil.equals(actionName, "replace")) {
+			actionInfo = new ActionInfo();
+
+			actionInfo.setActionKey(getReplaceActionKey());
+			actionInfo.setResourceMethodName(getReplaceResourceMethodName());
+		}
+
+		return actionInfo;
+	}
+
+}
