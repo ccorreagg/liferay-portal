64fef1eb34d765b40fff0490c3702126cc1ec954
Carlos Correa <carlos.correa@liferay.com>
2023-06-20 11:51:15 +0200

    WIP

--- modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/application/openapi/contributor/test/APIApplicationOpenAPIContributorTest.java
+++ modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/application/openapi/contributor/test/APIApplicationOpenAPIContributorTest.java
@@ -17,9 +17,17 @@ package com.liferay.headless.builder.application.openapi.contributor.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.batch.engine.unit.BatchEngineUnitProcessor;
 import com.liferay.batch.engine.unit.BatchEngineUnitReader;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.field.util.ObjectFieldUtil;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.HTTPTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
@@ -27,8 +35,11 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.vulcan.openapi.OpenAPIContext;
 import com.liferay.portal.vulcan.openapi.contributor.OpenAPIContributor;
 
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 import io.swagger.v3.oas.models.OpenAPI;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
@@ -56,7 +67,7 @@ public class APIApplicationOpenAPIContributorTest {
 		new LiferayIntegrationTestRule();
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		Bundle testBundle = FrameworkUtil.getBundle(
 			APIApplicationOpenAPIContributorTest.class);
 
@@ -74,6 +85,40 @@ public class APIApplicationOpenAPIContributorTest {
 				completableFuture.join();
 			}
 		}
+
+		_objectDefinition = _publishObjectDefinition(
+			Arrays.asList(
+				ObjectFieldUtil.createObjectField(
+					ObjectFieldConstants.BUSINESS_TYPE_DECIMAL, ObjectFieldConstants.DB_TYPE_DOUBLE, RandomTestUtil.randomString(), "decimalField", false),
+				ObjectFieldUtil.createObjectField(
+					ObjectFieldConstants.BUSINESS_TYPE_INTEGER, ObjectFieldConstants.DB_TYPE_INTEGER, RandomTestUtil.randomString(), "integerField", false),
+				ObjectFieldUtil.createObjectField(
+					ObjectFieldConstants.BUSINESS_TYPE_LONG_INTEGER, ObjectFieldConstants.DB_TYPE_LONG, RandomTestUtil.randomString(), "longIntegerField", false),
+				ObjectFieldUtil.createObjectField(
+					ObjectFieldConstants.BUSINESS_TYPE_PRECISION_DECIMAL, ObjectFieldConstants.DB_TYPE_BIG_DECIMAL, RandomTestUtil.randomString(), "precisionDecimal", false),
+				ObjectFieldUtil.createObjectField(
+					ObjectFieldConstants.BUSINESS_TYPE_TEXT, ObjectFieldConstants.DB_TYPE_STRING, RandomTestUtil.randomString(), "textField", false)));
+	}
+
+
+	private ObjectDefinition _publishObjectDefinition(
+		List<ObjectField> objectFields)
+		throws Exception {
+
+		long userId = TestPropsValues.getUserId();
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalServiceUtil.addCustomObjectDefinition(
+				userId, false, false,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				"A" + RandomTestUtil.randomString(), null, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				ObjectDefinitionConstants.SCOPE_COMPANY,
+				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+				objectFields);
+
+		return _objectDefinitionLocalServiceUtil.publishCustomObjectDefinition(
+			userId, objectDefinition.getObjectDefinitionId());
 	}
 
 	@After
@@ -109,20 +154,48 @@ public class APIApplicationOpenAPIContributorTest {
 				JSONUtil.put(
 					JSONUtil.put(
 						"apiSchemaToAPIProperties",
-						JSONUtil.put(
+						JSONUtil.putAll(
 							JSONUtil.put(
-								"description", "description"
+								"description", "decimalFieldDescription"
 							).put(
-								"name", "name"
+								"name", "decimalFieldName"
 							).put(
-								"objectFieldERC", "APPLICATION_STATUS"
+								"objectFieldERC", "decimalField"
+							),
+							JSONUtil.put(
+								"description", "textFieldDescription"
+							).put(
+								"name", "textFieldName"
+							).put(
+								"objectFieldERC", "textField"
+							),
+							JSONUtil.put(
+								"description", "textFieldDescription"
+							).put(
+								"name", "textFieldName"
+							).put(
+								"objectFieldERC", "textField"
+							),
+							JSONUtil.put(
+								"description", "textFieldDescription"
+							).put(
+								"name", "textFieldName"
+							).put(
+								"objectFieldERC", "textField"
+							),
+							JSONUtil.put(
+								"description", "textFieldDescription"
+							).put(
+								"name", "textFieldName"
+							).put(
+								"objectFieldERC", "textField"
 							))
 					).put(
 						"description", "description"
 					).put(
 						"externalReferenceCode", "SCHEMA"
 					).put(
-						"mainObjectDefinitionERC", "MSOD_API_APPLICATION"
+						"mainObjectDefinitionERC", _objectDefinition.getExternalReferenceCode()
 					).put(
 						"name", "name"
 					))
@@ -170,5 +243,10 @@ public class APIApplicationOpenAPIContributorTest {
 
 	@Inject
 	private BatchEngineUnitReader _batchEngineUnitReader;
+
+	private ObjectDefinition _objectDefinition;
+
+	@Inject
+	private ObjectDefinitionLocalServiceUtil _objectDefinitionLocalServiceUtil;
 
 }
