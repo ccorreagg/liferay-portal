28e4ef1a60ff35ddbbecdb748c06c1fd05bbd800
Carlos Correa <carlos.correa@liferay.com>
2023-10-31 16:37:06 +0100

    LPS-153494 Create a specific folder for each compatibilityVersion value. With that, we will be testing in the same execution that we are not breaking previous version

--- modules/util/portal-tools-rest-builder/src/test/java/com/liferay/portal/tools/rest/builder/RESTBuilderTest.java
+++ modules/util/portal-tools-rest-builder/src/test/java/com/liferay/portal/tools/rest/builder/RESTBuilderTest.java
@@ -5,6 +5,10 @@
 
 package com.liferay.portal.tools.rest.builder;
 
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.tools.rest.builder.internal.util.FileUtil;
+
+import java.io.File;
 import java.io.IOException;
 
 import java.net.URL;
@@ -30,13 +34,39 @@ public class RESTBuilderTest {
 
 		Path copyrightFilePath = dependenciesPath.resolve("copyright.txt");
 
-		RESTBuilder restBuilder = new RESTBuilder(
-			copyrightFilePath.toFile(), dependenciesPath.toFile(), null, null);
+		Path restConfigPath = dependenciesPath.resolve("rest-config.yaml");
 
-		restBuilder.build();
+		File restConfigFile = restConfigPath.toFile();
 
-		_assertDirectoryEquals(
-			dependenciesPath.resolve("expected"), _getActualPath());
+		String initialContent = FileUtil.read(restConfigFile);
+
+		for (int compatibilityVersion = 1;
+			 compatibilityVersion <= _MAX_COMPATIBILITY_VERSION;
+			 compatibilityVersion++) {
+
+			try {
+				FileUtil.write(
+					restConfigFile,
+					StringUtil.replace(
+						FileUtil.read(restConfigFile),
+						"${compatibilityVersion}",
+						String.valueOf(compatibilityVersion)));
+
+				RESTBuilder restBuilder = new RESTBuilder(
+					copyrightFilePath.toFile(), dependenciesPath.toFile(), null,
+					null);
+
+				restBuilder.build();
+
+				_assertDirectoryEquals(
+					dependenciesPath.resolve(
+						"expected/compatibilityVersion" + compatibilityVersion),
+					_getActualPath(compatibilityVersion));
+			}
+			finally {
+				FileUtil.write(restConfigFile, initialContent);
+			}
+		}
 	}
 
 	private void _assertDirectoryEquals(Path expectedPath, Path actualPath)
@@ -81,18 +111,22 @@ public class RESTBuilderTest {
 				public FileVisitResult visitFileFailed(
 					Path file, IOException ioException) {
 
+					Assert.fail("Missing file " + file.toAbsolutePath());
+
 					return FileVisitResult.TERMINATE;
 				}
 
 			});
 	}
 
-	private Path _getActualPath() {
+	private Path _getActualPath(int compatibilityVersion) {
 		Path path = Paths.get("");
 
 		Path absolutePath = path.toAbsolutePath();
 
-		return Paths.get(absolutePath.toString(), "test-classes", "actual");
+		return Paths.get(
+			absolutePath.toString(), "test-classes", "actual",
+			"compatibilityVersion" + compatibilityVersion);
 	}
 
 	private Path _getDependenciesPath() {
@@ -100,5 +134,7 @@ public class RESTBuilderTest {
 
 		return Paths.get(resource.getPath(), "dependencies");
 	}
+
+	private static final int _MAX_COMPATIBILITY_VERSION = 4;
 
 }
