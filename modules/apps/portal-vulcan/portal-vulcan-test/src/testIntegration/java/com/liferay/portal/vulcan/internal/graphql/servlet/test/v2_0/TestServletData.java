dbf83753d7dd403e66a62c8f1dccbcafda857651
Carlos Correa <carlos.correa@liferay.com>
2024-05-06 19:21:58 +0200

    LPD-24283 The test classes must be inside a package with the version, as in REST Builder that is the package pattern and it is used to know which is the version of each query/mutation

---
+++ modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/graphql/servlet/test/v2_0/TestServletData.java
@@ -0,0 +1,47 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.internal.graphql.servlet.test.v2_0;
+
+import com.liferay.portal.vulcan.graphql.servlet.ServletData;
+
+/**
+ * @author Carlos Correa
+ */
+public class TestServletData implements ServletData {
+
+	public TestServletData(TestDTO testDTO) {
+		_testQuery = new TestQuery(testDTO);
+	}
+
+	@Override
+	public String getApplicationName() {
+		return "test";
+	}
+
+	@Override
+	public Object getMutation() {
+		return _testMutationV1;
+	}
+
+	@Override
+	public String getPath() {
+		return "/test-path-graphql/v2_0";
+	}
+
+	@Override
+	public TestQuery getQuery() {
+		return _testQuery;
+	}
+
+	@Override
+	public boolean isJaxRsResourceInvocation() {
+		return false;
+	}
+
+	private final TestMutation _testMutationV1 = new TestMutation();
+	private final TestQuery _testQuery;
+
+}
