ae843ef965fb1975c71374dd3ba17bc97f2a2be4
Carlos Correa <carlos.correa@liferay.com>
2024-07-19 12:41:57 +0200

    TEST

--- modules/apps/dispatch/dispatch-talend-web/src/main/java/com/liferay/dispatch/talend/web/internal/executor/S7A3DispatchTaskExecutor.java
+++ modules/apps/dispatch/dispatch-talend-web/src/main/java/com/liferay/dispatch/talend/web/internal/executor/S7A3DispatchTaskExecutor.java
@@ -8,14 +8,23 @@ package com.liferay.dispatch.talend.web.internal.executor;
 import com.liferay.dispatch.executor.BaseDispatchTaskExecutor;
 import com.liferay.dispatch.executor.DispatchTaskExecutor;
 import com.liferay.dispatch.executor.DispatchTaskExecutorOutput;
+import com.liferay.dispatch.executor.DispatchTaskStatus;
+import com.liferay.dispatch.model.DispatchLog;
 import com.liferay.dispatch.model.DispatchTrigger;
+import com.liferay.dispatch.service.DispatchLogLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import com.liferay.portal.kernel.security.RandomUtil;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Carlos Correa
@@ -28,6 +37,12 @@ import org.osgi.service.component.annotations.Component;
 )
 public class S7A3DispatchTaskExecutor extends BaseDispatchTaskExecutor {
 
+	private final ScheduledExecutorService _scheduledExecutorService =
+		Executors.newScheduledThreadPool(1);
+
+	@Reference
+	private DispatchLogLocalService _dispatchLogLocalService;
+
 	@Override
 	public void doExecute(
 			DispatchTrigger dispatchTrigger,
@@ -38,6 +53,34 @@ public class S7A3DispatchTaskExecutor extends BaseDispatchTaskExecutor {
 			_log.info(
 				"Invoking #doExecute(DispatchTrigger, " +
 					"DispatchTaskExecutorOutput)");
+		}
+
+		_scheduledExecutorService.schedule(() -> {
+//			dispatchTaskExecutorOutput.setError("This is my error");
+
+			DispatchLog dispatchLog =
+				_dispatchLogLocalService.fetchLatestDispatchLog(
+					dispatchTrigger.getDispatchTriggerId(),
+					DispatchTaskStatus.IN_PROGRESS);
+
+			if (dispatchLog == null) {
+				if (_log.isInfoEnabled()) {
+					_log.info("Not possible");
+				}
+
+				return;
+			}
+
+			dispatchLog.setError("This is my status code");
+
+			_dispatchLogLocalService.updateDispatchLog(dispatchLog);
+
+		} , 1, TimeUnit.SECONDS);
+
+		try {
+			Thread.sleep(RandomUtil.nextInt(1200));
+		} catch (Exception exception) {
+
 		}
 	}
 
