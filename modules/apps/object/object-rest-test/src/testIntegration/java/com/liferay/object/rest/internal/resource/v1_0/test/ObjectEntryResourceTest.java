69e98cc1aadcb4db2005c827833fc33d48741528
Daniel Raposo <daniel.raposo@liferay.com>
2025-04-29 19:15:15 +0200

    LPD-53245 Remove ERC from PUT body to check its invariability

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -16913,9 +16913,7 @@ public class ObjectEntryResourceTest {
 			Http.Method.POST);
 
 		HTTPTestUtil.invokeToJSONObject(
-			JSONUtil.put(
-				"externalReferenceCode", _ERC_VALUE_1
-			).toString(),
+			JSONFactoryUtil.getNullJSON(),
 			objectDefinition1.getRESTContextPath() + "/" +
 				jsonObject.getLong("id"),
 			Http.Method.PUT);
@@ -17050,8 +17048,6 @@ public class ObjectEntryResourceTest {
 				).put(
 					objectRelationshipERCObjectFieldName,
 					_objectEntry2.getExternalReferenceCode()
-				).put(
-					"externalReferenceCode", _ERC_VALUE_1
 				).toString(),
 				objectDefinition1.getRESTContextPath() + "/" +
 					jsonObject.getLong("id"),
