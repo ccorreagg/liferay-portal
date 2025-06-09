5290c82321d488d69924c680654bc6ba55c01f0a
Carlos Correa <carlos.correa@liferay.com>
2025-06-09 12:46:43 +0200

    LPD-57341 Add test

---
+++ modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/dto/converter/test/DTOConverterRegistryTest.java
@@ -0,0 +1,201 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.vulcan.dto.converter.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Carlos Correa
+ */
+@RunWith(Arquillian.class)
+public class DTOConverterRegistryTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() {
+		Bundle bundle = FrameworkUtil.getBundle(DTOConverterRegistryTest.class);
+
+		_bundleContext = bundle.getBundleContext();
+	}
+
+	@Test
+	public void testGetDTOClassNames() throws Exception {
+		String dtoClassName = RandomTestUtil.randomString();
+
+		Set<String> dtoClassNames = _dtoConverterRegistry.getDTOClassNames();
+
+		Assert.assertFalse(dtoClassNames.contains(dtoClassName));
+
+		try (AutoCloseable autoCloseable = _registerDTOConverter(
+				null, null, dtoClassName, new TestDTOConverter(), null)) {
+
+			dtoClassNames = _dtoConverterRegistry.getDTOClassNames();
+
+			Assert.assertTrue(dtoClassNames.contains(dtoClassName));
+		}
+	}
+
+	@Test
+	public void testGetDTOConverterWithApplicationNameDefaultDTOClassNameAndVersionProperties()
+		throws Exception {
+
+		String applicationName = RandomTestUtil.randomString();
+		String dtoClassName = RandomTestUtil.randomString();
+		String version = RandomTestUtil.randomString();
+
+		DTOConverter<?, ?> dtoConverter1 = new TestDTOConverter();
+		DTOConverter<?, ?> dtoConverter2 = new TestDTOConverter();
+		DTOConverter<?, ?> dtoConverter3 = new TestDTOConverter();
+
+		try (AutoCloseable autoCloseable1 = _registerDTOConverter(
+				applicationName, null, dtoClassName, dtoConverter1, version);
+			AutoCloseable autoCloseable2 = _registerDTOConverter(
+				applicationName, false, dtoClassName, dtoConverter2, version);
+			AutoCloseable autoCloseable3 = _registerDTOConverter(
+				applicationName, true, dtoClassName, dtoConverter3, version)) {
+
+			Assert.assertSame(
+				dtoConverter3,
+				_dtoConverterRegistry.getDTOConverter(dtoClassName));
+
+			Assert.assertSame(
+				dtoConverter3,
+				_dtoConverterRegistry.getDTOConverter(
+					applicationName, dtoClassName, version));
+		}
+	}
+
+	@Test
+	public void testGetDTOConverterWithApplicationNameDTOClassNameAndVersionProperties()
+		throws Exception {
+
+		String applicationName = RandomTestUtil.randomString();
+		String dtoClassName = RandomTestUtil.randomString();
+		String version = RandomTestUtil.randomString();
+
+		DTOConverter<?, ?> dtoConverter = new TestDTOConverter();
+
+		try (AutoCloseable autoCloseable = _registerDTOConverter(
+				applicationName, null, dtoClassName, dtoConverter, version)) {
+
+			Assert.assertSame(
+				dtoConverter,
+				_dtoConverterRegistry.getDTOConverter(dtoClassName));
+
+			Assert.assertSame(
+				dtoConverter,
+				_dtoConverterRegistry.getDTOConverter(
+					applicationName, dtoClassName, version));
+		}
+	}
+
+	@Test
+	public void testGetDTOConverterWithDefaultAndDTOClassNameProperties()
+		throws Exception {
+
+		String dtoClassName = RandomTestUtil.randomString();
+		DTOConverter<?, ?> dtoConverter1 = new TestDTOConverter();
+		DTOConverter<?, ?> dtoConverter2 = new TestDTOConverter();
+		DTOConverter<?, ?> dtoConverter3 = new TestDTOConverter();
+
+		try (AutoCloseable autoCloseable1 = _registerDTOConverter(
+				null, null, dtoClassName, dtoConverter1, null);
+			AutoCloseable autoCloseable2 = _registerDTOConverter(
+				null, false, dtoClassName, dtoConverter2, null);
+			AutoCloseable autoCloseable3 = _registerDTOConverter(
+				null, true, dtoClassName, dtoConverter3, null)) {
+
+			Assert.assertSame(
+				dtoConverter3,
+				_dtoConverterRegistry.getDTOConverter(dtoClassName));
+		}
+	}
+
+	@Test
+	public void testGetDTOConverterWithDTOClassNameProperty() throws Exception {
+		String dtoClassName = RandomTestUtil.randomString();
+
+		Assert.assertNull(_dtoConverterRegistry.getDTOConverter(dtoClassName));
+
+		DTOConverter<?, ?> dtoConverter = new TestDTOConverter();
+
+		try (AutoCloseable autoCloseable = _registerDTOConverter(
+				null, null, dtoClassName, dtoConverter, null)) {
+
+			Assert.assertSame(
+				dtoConverter,
+				_dtoConverterRegistry.getDTOConverter(dtoClassName));
+		}
+	}
+
+	private AutoCloseable _registerDTOConverter(
+		String applicationName, Boolean defaultDTOConverter,
+		String dtoClassName, DTOConverter<?, ?> dtoConverter, String version) {
+
+		ServiceRegistration<DTOConverter<?, ?>> serviceRegistration =
+			_bundleContext.registerService(
+				(Class<DTOConverter<?, ?>>)(Class<?>)DTOConverter.class,
+				dtoConverter,
+				HashMapDictionaryBuilder.put(
+					"application.name", () -> applicationName
+				).put(
+					"default",
+					() -> {
+						if (defaultDTOConverter == null) {
+							return null;
+						}
+
+						return defaultDTOConverter.toString();
+					}
+				).put(
+					"dto.class.name", dtoClassName
+				).put(
+					"version", () -> version
+				).build());
+
+		return serviceRegistration::unregister;
+	}
+
+	private static BundleContext _bundleContext;
+
+	@Inject
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	private static class TestDTOConverter
+		implements DTOConverter<Object, Object> {
+
+		@Override
+		public String getContentType() {
+			return "";
+		}
+
+	}
+
+}
