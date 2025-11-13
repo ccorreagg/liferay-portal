9826cd7a313c8eda4cc1a4d52761515620d2d317
Alicia García <alicia.garcia@liferay.com>
2025-11-13 11:00:54 +0100

    LPD-69282 numberOfTaxonomyCategories is now needed to be initialized on test if is added to the getAdditionalAssertFieldNames method

--- modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-test/src/testIntegration/java/com/liferay/headless/admin/taxonomy/resource/v1_0/test/TaxonomyVocabularyResourceTest.java
+++ modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-test/src/testIntegration/java/com/liferay/headless/admin/taxonomy/resource/v1_0/test/TaxonomyVocabularyResourceTest.java
@@ -278,6 +278,7 @@ public class TaxonomyVocabularyResourceTest
 					RandomTestUtil.randomString());
 				multiValued = RandomTestUtil.randomBoolean();
 				name = RandomTestUtil.randomString();
+				numberOfTaxonomyCategories = 0;
 				siteId = testGroup.getGroupId();
 				visibilityType = VisibilityType.PUBLIC;
 			}
