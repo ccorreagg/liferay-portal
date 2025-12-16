7f2d769d2713547ea475354378cc4b9d744b46ec
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2025-12-16 19:17:14 -0300

    LPD-74645 Add AssetCategoryIds and AssetTagNames in the service context so when the user updates through OIDC it persists

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OIDCUserInfoProcessor.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OIDCUserInfoProcessor.java
@@ -5,6 +5,8 @@
 
 package com.liferay.portal.security.sso.openid.connect.internal;
 
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.expando.kernel.model.ExpandoColumn;
 import com.liferay.expando.kernel.model.ExpandoColumnConstants;
 import com.liferay.expando.kernel.model.ExpandoTable;
@@ -323,6 +325,12 @@ public class OIDCUserInfoProcessor {
 
 		Contact contact = user.getContact();
 
+		serviceContext.setAssetCategoryIds(
+			_assetCategoryLocalService.getCategoryIds(
+				User.class.getName(), user.getUserId()));
+		serviceContext.setAssetTagNames(
+			_assetTagLocalService.getTagNames(
+				User.class.getName(), user.getUserId()));
 		serviceContext.setUuid(user.getUuid());
 
 		_addOrUpdateUserCustomClaims(
@@ -882,6 +890,12 @@ public class OIDCUserInfoProcessor {
 
 	@Reference
 	private AddressLocalService _addressLocalService;
+
+	@Reference
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Reference
+	private AssetTagLocalService _assetTagLocalService;
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
