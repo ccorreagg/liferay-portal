df95c7756bc7d8df522185a2a98a08067066aee1
Brian Chan <brian.chan@liferay.com>
2025-12-17 10:40:48 +0800

    LPD-73738 Simplify

--- portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyDatabaseState.java
+++ portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyDatabaseState.java
@@ -152,21 +152,14 @@ public class PreupgradeVerifyDatabaseState extends PreupgradeVerifyProcess {
 		return viewNames;
 	}
 
-	private static final Set<String> _falsePositive74UpgradeDroppedTableNames;
-
-	static {
-		_falsePositive74UpgradeDroppedTableNames = new TreeSet<>(
-			String.CASE_INSENSITIVE_ORDER);
-
-		_falsePositive74UpgradeDroppedTableNames.addAll(
-			Set.of(
-				"Account_", "AccountGroupAccountEntryRel",
-				"AssetEntries_AssetCategories", "BlogsStatsUser",
-				"CAccountGroupCAccountRel", "CommerceAccount",
-				"CommerceAccountGroup", "CommerceAccountGroupRel",
-				"CommerceAccountOrganizationRel", "CommerceAccountUserRel",
-				"CommerceAddress", "CommerceCountry", "CommerceRegion",
-				"MBStatsUser", "OrgGroupRole", "RemoteAppEntry"));
-	}
+	private static final Set<String> _falsePositive74UpgradeDroppedTableNames =
+		Set.of(
+			"Account_", "AccountGroupAccountEntryRel",
+			"AssetEntries_AssetCategories", "BlogsStatsUser",
+			"CAccountGroupCAccountRel", "CommerceAccount",
+			"CommerceAccountGroup", "CommerceAccountGroupRel",
+			"CommerceAccountOrganizationRel", "CommerceAccountUserRel",
+			"CommerceAddress", "CommerceCountry", "CommerceRegion",
+			"MBStatsUser", "OrgGroupRole", "RemoteAppEntry");
 
 }
