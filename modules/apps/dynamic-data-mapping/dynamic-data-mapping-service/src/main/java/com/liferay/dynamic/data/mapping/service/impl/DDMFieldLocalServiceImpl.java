597c1be70fe5ffefcce825a12ad68b19c2352847
Marco Galluzzi <marco.galluzzi@liferay.com>
2026-01-22 13:13:21 +0000

    LPD-76501 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFieldLocalServiceImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFieldLocalServiceImpl.java
@@ -868,7 +868,7 @@ public class DDMFieldLocalServiceImpl extends DDMFieldLocalServiceBaseImpl {
 			}
 
 			return new String[] {
-				languageId, defaultLanguageId, StringPool.BLANK
+				defaultLanguageId, languageId, StringPool.BLANK
 			};
 		}
 		catch (PortalException portalException) {
