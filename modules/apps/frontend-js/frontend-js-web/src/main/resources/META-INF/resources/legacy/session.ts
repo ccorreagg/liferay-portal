54b77c6891138a7625747df5885228c089484091
antonio-ortega <antonio.ortega@liferay.com>
2026-01-29 08:43:38 +0100

    LPD-77703 We only need to removeAlert if it exits, and once we do it, we need to update sessionState

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/legacy/session.ts
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/legacy/session.ts
@@ -275,14 +275,16 @@ export class Session {
 				this.warn();
 			}
 
-			if (!shouldWarn) {
-				this._removeAlert();
-			}
-			else if (!shouldExpire) {
-				this._uiSetRemainingTime(
-					this.sessionLength - elapsed,
-					document.querySelector(`#${TOAST_ID} .countdown-timer`)
-				);
+			if (this._banner) {
+				if (!shouldWarn) {
+					this._removeAlert();
+				}
+				else if (!shouldExpire) {
+					this._uiSetRemainingTime(
+						this.sessionLength - elapsed,
+						document.querySelector(`#${TOAST_ID} .countdown-timer`)
+					);
+				}
 			}
 		}, 1000);
 	}
@@ -293,6 +295,8 @@ export class Session {
 		if (this._banner) {
 			this._destroyBanner();
 		}
+
+		this.sessionState = 'active';
 	}
 
 	private _uiSetExpired() {
