ec7e97a0071f1bf3e5b542515fd90f7a4f226af5
Javier Moral <javier.moral@liferay.com>
2026-02-10 13:29:08 +0100

    LPD-77079 Add Override annotation
    (cherry picked from commit 2cb92d3a2993457ab4367e7cc3ffc38331c37457)

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/util/configuration/FragmentEntryConfigurationParserImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/util/configuration/FragmentEntryConfigurationParserImpl.java
@@ -300,6 +300,7 @@ public class FragmentEntryConfigurationParserImpl
 		return null;
 	}
 
+	@Override
 	public Object getFieldValue(
 		JSONObject configurationJSONObject, JSONObject editableValuesJSONObject,
 		String name) {
