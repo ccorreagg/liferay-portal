246e6a67f2593f8e085a911e914dad949b216b7d
Brian Chan <brian.chan@liferay.com>
2026-02-19 11:26:31 -0300

    LPD-78079 Sort because we're in the REST world

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/resource/v1_0/AccountResourceImpl.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/resource/v1_0/AccountResourceImpl.java
@@ -648,7 +648,7 @@ public class AccountResourceImpl
 	}
 
 	private void _addAddresses(
-			Long accountId, Account account, AccountEntry accountEntry)
+			Account account, Long accountId, AccountEntry accountEntry)
 		throws Exception {
 
 		PostalAddress[] postalAddresses = account.getPostalAddresses();
@@ -1329,7 +1329,7 @@ public class AccountResourceImpl
 			Account account, AccountEntry accountEntry, Long accountId)
 		throws Exception {
 
-		_addAddresses(accountId, account, accountEntry);
+		_addAddresses(account, accountId, accountEntry);
 
 		accountEntry = _accountEntryLocalService.updateDefaultBillingAddressId(
 			accountId,
