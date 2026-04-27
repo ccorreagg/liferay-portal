4fcb16af39fec1fd1142f9d761295c49496f86bb
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-04-27 13:30:40 +0200

    LPD-87493 Adds tests

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -8261,12 +8261,12 @@ public class ObjectEntryResourceTest {
 			Http.Method.GET);
 
 		Assert.assertEquals(
-			jsonObject.get("externalReferenceCode"),
+			jsonObject.getString("externalReferenceCode"),
 			jsonObject.getString("friendlyUrlPath"));
 
 		jsonObject = HTTPTestUtil.invokeToJSONObject(
 			JSONUtil.put(
-				"friendlyUrlPath", "Test URL"
+				"friendlyUrlPath", StringPool.SLASH
 			).toString(),
 			_objectDefinition1.getRESTContextPath(), Http.Method.POST);
 
@@ -8277,7 +8277,12 @@ public class ObjectEntryResourceTest {
 			Http.Method.GET);
 
 		Assert.assertEquals(
-			"test-url", jsonObject.getString("friendlyUrlPath"));
+			jsonObject.getString("externalReferenceCode"),
+			jsonObject.getString("friendlyUrlPath"));
+
+		_assertGetObjectEntryWithFriendlyURL("hello-world", "/hello-world");
+		_assertGetObjectEntryWithFriendlyURL("parent/child", "parent//child");
+		_assertGetObjectEntryWithFriendlyURL("test-url", "Test URL");
 	}
 
 	@Test
@@ -16020,6 +16025,26 @@ public class ObjectEntryResourceTest {
 		Assert.assertEquals(
 			String.valueOf(expectedObjectFieldValue),
 			String.valueOf(itemJSONObject.get(expectedObjectFieldName)));
+	}
+
+	private void _assertGetObjectEntryWithFriendlyURL(
+			String expectedFriendlyUrlPath, String friendlyUrlPath)
+		throws Exception {
+
+		JSONObject jsonObject = HTTPTestUtil.invokeToJSONObject(
+			JSONUtil.put(
+				"friendlyUrlPath", friendlyUrlPath
+			).toString(),
+			_objectDefinition1.getRESTContextPath(), Http.Method.POST);
+
+		jsonObject = HTTPTestUtil.invokeToJSONObject(
+			null,
+			_objectDefinition1.getRESTContextPath() + StringPool.SLASH +
+				jsonObject.getString("id"),
+			Http.Method.GET);
+
+		Assert.assertEquals(
+			expectedFriendlyUrlPath, jsonObject.getString("friendlyUrlPath"));
 	}
 
 	private void _assertInvocations(
