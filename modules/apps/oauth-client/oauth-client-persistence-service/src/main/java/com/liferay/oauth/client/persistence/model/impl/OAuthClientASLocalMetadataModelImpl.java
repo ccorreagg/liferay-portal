3f3907976a302ca54d4ac66bde0e9b9af42df024
Manuele Castro <manuele.castro@liferay.com>
2026-05-15 13:50:07 -0300

    LPD-89144 buildService

--- modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/model/impl/OAuthClientASLocalMetadataModelImpl.java
+++ modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/model/impl/OAuthClientASLocalMetadataModelImpl.java
@@ -99,7 +99,7 @@ public class OAuthClientASLocalMetadataModelImpl
 	}
 
 	public static final String TABLE_SQL_CREATE =
-		"create table OAuthClientASLocalMetadata (mvccVersion LONG default 0 not null,uuid_ VARCHAR(75) null,externalReferenceCode VARCHAR(75) null,oAuthClientASLocalMetadataId LONG not null primary key,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,issuer VARCHAR(75) null,localWellKnownEnabled BOOLEAN,localWellKnownURI VARCHAR(256) null,metadataJSON TEXT null,oAuthASLocalWellKnownURI VARCHAR(256) null,oAuthASMetadataJSON TEXT null)";
+		"create table OAuthClientASLocalMetadata (mvccVersion LONG default 0 not null,uuid_ VARCHAR(75) null,externalReferenceCode VARCHAR(75) null,oAuthClientASLocalMetadataId LONG not null primary key,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,issuer VARCHAR(256) null,localWellKnownEnabled BOOLEAN,localWellKnownURI VARCHAR(256) null,metadataJSON TEXT null,oAuthASLocalWellKnownURI VARCHAR(256) null,oAuthASMetadataJSON TEXT null)";
 
 	public static final String TABLE_SQL_DROP =
 		"drop table OAuthClientASLocalMetadata";
@@ -1289,4 +1289,4 @@ public class OAuthClientASLocalMetadataModelImpl
 	private OAuthClientASLocalMetadata _escapedModel;
 
 }
-// LIFERAY-SERVICE-BUILDER-HASH:1157404049
+// LIFERAY-SERVICE-BUILDER-HASH:1631620134
