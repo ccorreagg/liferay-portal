a87b3795819299d4c8af80a1ba7657c21602f645
Alicia García <alicia.garcia@liferay.com>
2026-05-22 12:12:35 +0200

    LPD-91298 add STATUS_EXPIRED to do not check neither the expired ones

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3177,6 +3177,7 @@ public class ObjectEntryLocalServiceImpl
 					ObjectEntryTable.INSTANCE.status.notIn(
 						new Integer[] {
 							WorkflowConstants.STATUS_DRAFT,
+							WorkflowConstants.STATUS_EXPIRED,
 							WorkflowConstants.STATUS_IN_TRASH,
 							WorkflowConstants.STATUS_PENDING
 						})
