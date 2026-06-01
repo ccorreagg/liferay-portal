85d0f6a79da9dc5621aad22b58c46135d5575c61
Brian Wulbern <brian.wulbern@liferay.com>
2026-06-01 17:11:53 -0500

    Revert "LPD-85590 Move the request-attribute assert inside the try-with-resources"
    This reverts commit 42c2348a76b9f8cb4dfa2cf456ba17fd5de09523.

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
@@ -854,15 +854,14 @@ public class PortalImplUnitTest {
 				0, exception, mockHttpServletRequest,
 				new MockHttpServletResponse());
 
-			Assert.assertSame(
-				exception,
-				mockHttpServletRequest.getAttribute(
-					WebKeys.PORTAL_STATUS_EXCEPTION));
-
 			sessionErrorsMockedStatic.verify(
 				() -> SessionErrors.add(
 					mockHttpSession, Exception.class, exception));
 		}
+
+		Assert.assertSame(
+			exception,
+			mockHttpServletRequest.getAttribute(WebKeys.PORTAL_STATUS_EXCEPTION));
 	}
 
 	@Test
