79a161a42867377a8926402d2f29e91f9ca36884
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-06-03 15:00:25 +0200

    LPD-93228 Pass breadcrumb props to the Recent Assets section

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewHomeRecentAssetsSectionDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewHomeRecentAssetsSectionDisplayContext.java
@@ -12,8 +12,13 @@ import com.liferay.object.model.ObjectEntryFolder;
 import com.liferay.object.service.ObjectDefinitionService;
 import com.liferay.object.service.ObjectDefinitionSettingLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
@@ -53,11 +58,45 @@ public class ViewHomeRecentAssetsSectionDisplayContext
 			translationInfoItemFieldValuesExporterRegistry);
 	}
 
+	@Override
+	public Map<String, Object> getAdditionalProps() {
+		Map<String, Object> additionalProps = super.getAdditionalProps();
+
+		try {
+			additionalProps.put("breadcrumbProps", getBreadcrumbProps());
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+		}
+
+		return additionalProps;
+	}
+
 	public String getAssetsAllURL() throws PortalException {
 		return PortalUtil.getLayoutFullURL(
 			LayoutLocalServiceUtil.getLayoutByFriendlyURL(
 				themeDisplay.getScopeGroupId(), false, "/all"),
 			themeDisplay);
+	}
+
+	@Override
+	public Map<String, Object> getBreadcrumbProps() throws PortalException {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		Layout layout = LayoutLocalServiceUtil.getLayoutByFriendlyURL(
+			themeDisplay.getScopeGroupId(), false, "/all");
+
+		addBreadcrumbItem(
+			jsonArray, false, null,
+			layout.getName(themeDisplay.getLocale(), true));
+
+		return HashMapBuilder.<String, Object>put(
+			"breadcrumbItems", jsonArray
+		).put(
+			"hideSpace", true
+		).build();
 	}
 
 	@Override
@@ -93,5 +132,8 @@ public class ViewHomeRecentAssetsSectionDisplayContext
 				"cmsKind eq 'object' and (cmsSection eq 'contents' or " +
 					"cmsSection eq 'files')"));
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ViewHomeRecentAssetsSectionDisplayContext.class);
 
 }
