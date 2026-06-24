5a31ea976980e08cc2fcd79d16c2a2929eef5c5d
joshitagaki <joshua.itagaki@liferay.com>
2026-06-24 08:09:05 -0700

    "LPD-95692 Revert LPD-89720 Return Space-scoped tags when filtering CMS tags by Space"
    This reverts commit 6226c623d20d9d7eef6653d5c84b747046d238b7.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/categorization/tags/ViewTags.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/categorization/tags/ViewTags.tsx
@@ -68,7 +68,7 @@ export default function ViewTags({
 			apiURL: '/o/headless-asset-library/v1.0/asset-libraries',
 			entityFieldType: 'string',
 			id: 'groupIds',
-			itemKey: 'siteId',
+			itemKey: 'id',
 			itemLabel: 'name',
 			label: 'Space',
 			multiple: true,
