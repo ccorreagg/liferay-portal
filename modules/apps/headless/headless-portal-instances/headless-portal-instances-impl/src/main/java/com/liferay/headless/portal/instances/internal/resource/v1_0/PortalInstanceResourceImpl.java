7edf9ee07ea4c0da0aa22f10286b2edb9bfa8f53
Luis Ortiz <luis.ortiz@liferay.com>
2026-09-18 14:42:30 +0200

    LPD-93380 Move the copy, export and import operations to their resources
    Each one now reads the instance id from its payload instead of the path, and declares the administrator credential as sensitive so the batch engine protects it.

--- modules/apps/headless/headless-portal-instances/headless-portal-instances-impl/src/main/java/com/liferay/headless/portal/instances/internal/resource/v1_0/PortalInstanceResourceImpl.java
+++ modules/apps/headless/headless-portal-instances/headless-portal-instances-impl/src/main/java/com/liferay/headless/portal/instances/internal/resource/v1_0/PortalInstanceResourceImpl.java
@@ -7,16 +7,10 @@ package com.liferay.headless.portal.instances.internal.resource.v1_0;
 
 import com.liferay.headless.portal.instances.dto.v1_0.Admin;
 import com.liferay.headless.portal.instances.dto.v1_0.PortalInstance;
-import com.liferay.headless.portal.instances.dto.v1_0.PortalInstanceCopy;
-import com.liferay.headless.portal.instances.dto.v1_0.PortalInstanceExport;
-import com.liferay.headless.portal.instances.dto.v1_0.PortalInstanceImport;
 import com.liferay.headless.portal.instances.resource.v1_0.PortalInstanceResource;
-import com.liferay.portal.instances.exporter.PortalInstanceExporter;
 import com.liferay.portal.kernel.exception.UserEmailAddressException;
 import com.liferay.portal.kernel.exception.UserScreenNameException;
 import com.liferay.portal.kernel.instance.PortalInstancePool;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.security.auth.EmailAddressValidator;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
@@ -28,8 +22,6 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.auth.EmailAddressValidatorFactory;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.vulcan.pagination.Page;
-
-import jakarta.ws.rs.BadRequestException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -125,6 +117,10 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 
 		long finalCompanyId = companyId;
 
+		boolean active = GetterUtil.getBoolean(
+			portalInstance.getActive(), true);
+		int maxUsers = GetterUtil.getInteger(portalInstance.getMaxUsers());
+
 		if (admin != null) {
 			_validateAdmin(admin);
 
@@ -134,9 +130,10 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 					() -> _companyService.addCompany(
 						finalCompanyId, portalInstance.getPortalInstanceId(),
 						portalInstance.getVirtualHost(),
-						portalInstance.getDomain(), 0, true, null, null,
-						admin.getEmailAddress(), admin.getGivenName(), null,
-						admin.getFamilyName())));
+						portalInstance.getDomain(), maxUsers, active,
+						admin.getPassword(), admin.getScreenName(),
+						admin.getEmailAddress(), admin.getGivenName(),
+						admin.getMiddleName(), admin.getFamilyName())));
 		}
 
 		return _toPortalInstance(
@@ -145,122 +142,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 				() -> _companyService.addCompany(
 					finalCompanyId, portalInstance.getPortalInstanceId(),
 					portalInstance.getVirtualHost(), portalInstance.getDomain(),
-					0, true)));
-	}
-
-	@Override
-	public PortalInstance postPortalInstanceCopy(
-			String portalInstanceId, PortalInstanceCopy portalInstanceCopy)
-		throws Exception {
-
-		_checkPermission();
-
-		if (portalInstanceCopy == null) {
-			throw new BadRequestException("Copy configuration is required");
-		}
-
-		if (Validator.isNull(portalInstanceCopy.getName())) {
-			throw new BadRequestException("Name is required");
-		}
-
-		if (Validator.isNull(portalInstanceCopy.getVirtualHost())) {
-			throw new BadRequestException("Virtual host is required");
-		}
-
-		if (Validator.isNull(portalInstanceCopy.getWebId())) {
-			throw new BadRequestException("Web ID is required");
-		}
-
-		Company fromCompany = _companyService.getCompanyByWebId(
-			portalInstanceId);
-
-		Long toCompanyId = portalInstanceCopy.getDestinationCompanyId();
-
-		if ((toCompanyId != null) && (toCompanyId <= 0)) {
-			toCompanyId = null;
-		}
-
-		try {
-			return _toPortalInstance(
-				_companyService.copyDBPartitionCompany(
-					fromCompany.getCompanyId(), toCompanyId,
-					portalInstanceCopy.getName(),
-					portalInstanceCopy.getVirtualHost(),
-					portalInstanceCopy.getWebId()));
-		}
-		catch (Exception exception) {
-			_log.error(
-				"Unable to copy portal instance " + portalInstanceId,
-				exception);
-
-			throw exception;
-		}
-	}
-
-	@Override
-	public PortalInstanceExport postPortalInstanceExport(
-			String portalInstanceId)
-		throws Exception {
-
-		_checkPermission();
-
-		try {
-			PortalInstanceExport portalInstanceExport =
-				new PortalInstanceExport();
-
-			Company company = _companyService.getCompanyByWebId(
-				portalInstanceId);
-
-			String exportedPartitionName =
-				_portalInstanceExporter.exportPortalInstance(
-					company.getCompanyId());
-
-			portalInstanceExport.setExportedPartitionName(
-				() -> exportedPartitionName);
-
-			portalInstanceExport.setSourceCompanyId(company::getCompanyId);
-
-			return portalInstanceExport;
-		}
-		catch (Exception exception) {
-			_log.error(
-				"Unable to export portal instance " + portalInstanceId,
-				exception);
-
-			throw exception;
-		}
-	}
-
-	@Override
-	public PortalInstance postPortalInstanceImport(
-			PortalInstanceImport portalInstanceImport)
-		throws Exception {
-
-		_checkPermission();
-
-		if (portalInstanceImport == null) {
-			throw new BadRequestException("Import configuration is required");
-		}
-
-		String schemaName = portalInstanceImport.getSchemaName();
-
-		if (Validator.isNull(schemaName)) {
-			throw new BadRequestException("Schema name is required");
-		}
-
-		try {
-			return _toPortalInstance(
-				_companyService.addDBPartitionCompany(
-					schemaName, portalInstanceImport.getName(),
-					portalInstanceImport.getVirtualHost(),
-					portalInstanceImport.getWebId()));
-		}
-		catch (Exception exception) {
-			_log.error(
-				"Unable to import portal instance " + schemaName, exception);
-
-			throw exception;
-		}
+					maxUsers, active)));
 	}
 
 	@Override
@@ -304,6 +186,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 				setActive(company::isActive);
 				setCompanyId(company::getCompanyId);
 				setDomain(company::getMx);
+				setMaxUsers(company::getMaxUsers);
 				setPortalInstanceId(company::getWebId);
 				setVirtualHost(company::getVirtualHostname);
 			}
@@ -327,13 +210,7 @@ public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
 		}
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		PortalInstanceResourceImpl.class);
-
 	@Reference
 	private CompanyService _companyService;
-
-	@Reference
-	private PortalInstanceExporter _portalInstanceExporter;
 
 }
