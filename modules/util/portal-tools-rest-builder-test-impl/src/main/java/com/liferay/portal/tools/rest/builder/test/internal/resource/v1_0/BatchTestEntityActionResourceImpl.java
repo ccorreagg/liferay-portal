fc3df7db803308ab4f9523d6ec155421cc8a07cb
Carlos Correa <carlos.correa@liferay.com>
2026-09-23 17:42:57 +0200

    LPD-93380 Cover the operation whose schema already names its parent
    Implement the test API and add tests that validate the action calls both through the endpoint and through batch.

--- modules/util/portal-tools-rest-builder-test-impl/src/main/java/com/liferay/portal/tools/rest/builder/test/internal/resource/v1_0/BatchTestEntityActionResourceImpl.java
+++ modules/util/portal-tools-rest-builder-test-impl/src/main/java/com/liferay/portal/tools/rest/builder/test/internal/resource/v1_0/BatchTestEntityActionResourceImpl.java
@@ -5,13 +5,16 @@
 
 package com.liferay.portal.tools.rest.builder.test.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.tools.rest.builder.test.dto.v1_0.BatchTestEntityAction;
 import com.liferay.portal.tools.rest.builder.test.resource.v1_0.BatchTestEntityActionResource;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
- * @author Alejandro Tardín
+ * @author Carlos Correa
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/batch-test-entity-action.properties",
@@ -20,4 +23,21 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class BatchTestEntityActionResourceImpl
 	extends BaseBatchTestEntityActionResourceImpl {
+
+	@Override
+	public BatchTestEntityAction postBatchTestEntityAction(
+		BatchTestEntityAction batchTestEntityAction) {
+
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				"Posting batch test entity action " +
+					batchTestEntityAction.getName());
+		}
+
+		return batchTestEntityAction;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BatchTestEntityActionResourceImpl.class);
+
 }
